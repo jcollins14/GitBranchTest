25a7dd46509d9d1a6bc9aa205dc706529d38d9ac
Jake Collins <jakecollins1014@gmail.com>
2020-02-19 20:24:25 -0500

    init commit

---
+++ GitBranchTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitBranchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the master branch");
+        }
+    }
+}
