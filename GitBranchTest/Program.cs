24bf01ffff02e9c19584696f3c61691120cebff8
Jake Collins <jakecollins1014@gmail.com>
2020-02-19 21:06:00 -0500

    continued

--- GitBranchTest/Program.cs
+++ GitBranchTest/Program.cs
@@ -9,6 +9,7 @@ namespace GitBranchTest
             Console.WriteLine("This is the Zavala branch");
             Console.WriteLine("Whether we wanted it or not, we've stepped into a war with the Cabal on Mars");
             Console.WriteLine("So let's get to taking out their command, one by one.");
+            Console.WriteLine("Valus Ta'aurc. From what I can gather, he commands the Siege Dancers from an Imperial Land Tank just outside of Rubicon.");
         }
     }
 }
